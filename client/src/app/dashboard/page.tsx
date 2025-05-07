d22c5d69fe075acd8f95c91a6d8009380135f2ba
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-05-06 20:42:55 -0500

    feat: implemented drizzle (#6)
    * feat: implemented drizzle
    
    * chore: example usage of db methods
    
    * chore: added .env.exmaple

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import { db } from '@/db';
+import { users } from '@/db/schema';
 import RedditLeadCard from '@/lib/components/lead/card';
 
-export default function Students() {
+export default async function Students() {
+    const usersData = await db.select().from(users);
+
+    const firstUser = usersData.at(0);
+
+    console.log('first user: ', firstUser);
+
     return (
         <div className="w-2/3">
             <div className="w-full p-4 justify-center grid grid-cols-3 gap-2">
