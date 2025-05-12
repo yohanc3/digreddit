e6310ee69988b5276b8ea77036ed05068a3152a8
Yohance Williams-Rodriguez <balamyohance@gmail.com>
2025-05-11 22:43:48 -0500

    quick cleanup

--- server/routes.ts
+++ server/routes.ts
@@ -7,17 +7,7 @@
 |
 */
 
-import DynamicAhoCorasick from '#services/AhoCorasick'
-import { HttpContext } from '@adonisjs/core/http'
 import router from '@adonisjs/core/services/router'
 const WebhookIntake = () => import('#controllers/webhook')
 
 router.post('/webhook/intake', [WebhookIntake, 'intake'])
-router.get('/', async ({ response }: HttpContext) => {
-    console.log('request went through')
-
-    const aho = DynamicAhoCorasick.getInstance()
-    const keys = aho.getKeywords()
-
-    return response.status(200).send({ keywords: keys })
-})
