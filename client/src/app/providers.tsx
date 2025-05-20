e6b23229544d9d4127d39fea2781abeb61d0181d
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-05-19 21:28:22 -0500

    wip: dashboard read, create/delete product are left (#13)

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -4,7 +4,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
 export default function Providers({ children }: { children: ReactNode }) {
-    const queryClient = new QueryClient();
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                //refetch every 10 seconds
+                refetchInterval: 10 * 1000,
+                refetchOnWindowFocus: true,
+            },
+        },
+    });
 
     return (
         <QueryClientProvider client={queryClient}>
--- client/src/lib/components/ui/lead/list.tsx
+++ client/src/lib/components/ui/lead/list.tsx
@@ -2,6 +2,7 @@
 
 import clsx from 'clsx';
 import RedditLeadListItem from './item';
+import { useState } from 'react';
 
 interface RedditLeadListProps {
     productsList: { title: string; date: string }[];
@@ -14,17 +15,29 @@ export default function RedditLeadList({
     productsList,
     onSelectedProductChange,
 }: RedditLeadListProps) {
+    const [selectedItemIdx, setSelecetdItemIdx] = useState<number>(0);
+
     return (
         <div
-            className={clsx('h-1/2 text-secondarySize font-normal', className)}
+            className={clsx(
+                'h-full text-secondarySize font-normal flex flex-col gap-y-2',
+                className
+            )}
         >
             {productsList.map((product, index) => {
                 return (
                     <div
-                        onClick={() => onSelectedProductChange(index)}
+                        onClick={() => {
+                            onSelectedProductChange(index);
+                            setSelecetdItemIdx(index);
+                        }}
                         key={index}
                     >
                         <RedditLeadListItem
+                            className={clsx({
+                                'bg-[#73b3bb30] hover:bg-[#73b3bb30]':
+                                    selectedItemIdx === index,
+                            })}
                             title={product.title}
                             date={product.date}
                         />
