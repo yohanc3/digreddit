787b0b9927e337e2847ba93a5ffb267e87768e9d
jcomrade <jjac8115@gmail.com>
2025-05-27 01:42:39 +0800

    fix: error toast for failed products fetching

--- client/src/lib/frontend/hooks/useProducts.ts
+++ client/src/lib/frontend/hooks/useProducts.ts
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { useFetch } from "./useFetch";
+import { toast } from '@/hooks/use-toast';
+import { BiCheckCircle, BiErrorCircle } from 'react-icons/bi';
 
 export function useProducts() {
     const {apiGet} = useFetch()
@@ -11,8 +13,13 @@ export function useProducts() {
 
                 return result.userProducts;
             } catch (e) {
+                toast({
+                    variant: "destructive",
+                    title: 'Error',
+                    description: 'Something went wrong when fetching your Products List.'
+                });
                 console.error(
-                    'Error when fetching all user Products',
+                    'Error when fetching user Products',
                     e
                 );
                 return [];
