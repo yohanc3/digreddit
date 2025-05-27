b910e7b86635d585cfa848c9f3bd337fd54af6a4
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-05-26 22:03:44 -0500

    feat: leads sorting and filtering (#22)
    * feat: applied all filters and sorting methods
    
    * feat: added interaction flag and backend flow

--- client/drizzle/relations.ts
+++ client/drizzle/relations.ts
@@ -1,16 +1,18 @@
 import { relations } from "drizzle-orm/relations";
-import { products, commentLeads, postLeads, users, session, account, authenticator } from "./schema";
+import { users, feedback, products, postLeads, commentLeads, session, account, authenticator } from "./schema";
 
-export const commentLeadsRelations = relations(commentLeads, ({one}) => ({
-	product: one(products, {
-		fields: [commentLeads.productId],
-		references: [products.id]
+export const feedbackRelations = relations(feedback, ({one}) => ({
+	user: one(users, {
+		fields: [feedback.userId],
+		references: [users.id]
 	}),
 }));
 
-export const productsRelations = relations(products, ({many}) => ({
-	commentLeads: many(commentLeads),
-	postLeads: many(postLeads),
+export const usersRelations = relations(users, ({many}) => ({
+	feedbacks: many(feedback),
+	sessions: many(session),
+	accounts: many(account),
+	authenticators: many(authenticator),
 }));
 
 export const postLeadsRelations = relations(postLeads, ({one}) => ({
@@ -20,17 +22,23 @@ export const postLeadsRelations = relations(postLeads, ({one}) => ({
 	}),
 }));
 
+export const productsRelations = relations(products, ({many}) => ({
+	postLeads: many(postLeads),
+	commentLeads: many(commentLeads),
+}));
+
+export const commentLeadsRelations = relations(commentLeads, ({one}) => ({
+	product: one(products, {
+		fields: [commentLeads.productId],
+		references: [products.id]
+	}),
+}));
+
 export const sessionRelations = relations(session, ({one}) => ({
 	user: one(users, {
 		fields: [session.userId],
 		references: [users.id]
 	}),
-}));
-
-export const usersRelations = relations(users, ({many}) => ({
-	sessions: many(session),
-	accounts: many(account),
-	authenticators: many(authenticator),
 }));
 
 export const accountRelations = relations(account, ({one}) => ({
