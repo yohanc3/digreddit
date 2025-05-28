82d0ec24694f36ad844fa3607d43cbab849e9178
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-05-27 19:39:01 -0500

    fix: wrong interval, new db, new server env file (#27)

--- server/app/services/AhoCorasick.ts
+++ server/app/services/AhoCorasick.ts
@@ -111,7 +111,7 @@ export default class DynamicAhoCorasick {
         this.instance.keywords = [...DBKeywords]
 
         // Run the adding and removing keywords from the AhoCora every 5 minutes
-        const interval = 60 * 5
+        const interval = 60 * 5 * 1000
         console.log(`running adding and removing keywords every ${interval / 60} minutes...`)
         setInterval(async () => {
             await this.addKeywords()
