502e46e9c2331cf516bcfb86eb309c2d13ced5cf
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2025-06-06 18:07:49 -0500

    feat: get rid of sidebars and make dashboard prettier (#51)

--- client/src/app/your-products/layout.tsx
+++ client/src/app/your-products/layout.tsx
@@ -1,21 +1,14 @@
 import NavBar from '@/lib/components/ui/header/NavBar';
-import {
-    LeftSideBarLeadResult,
-    RightSideBarLeadResult,
-} from '@/lib/components/ui/lead/sidebar';
 
 export default function DashboardLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
     return (
         <div className="flex flex-col w-full">
             <NavBar />
-            <div className="h-full w-full flex justify-center">
-                {children}
-            </div>
+            <div className="h-full w-full flex justify-center">{children}</div>
         </div>
     );
 }
